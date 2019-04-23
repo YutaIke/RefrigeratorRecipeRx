f2b24658b11405d05c5c4c95968019003a1cf5a2
YutaIke <kei.yt22@gmail.com>
2019-04-23 23:19:30 +0900

    初期プロジェクト作成

---
+++ RefrigeratorRecipeRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RefrigeratorRecipeRx
+//
+//  Created by 池友太 on 2019/04/23.
+//  Copyright © 2019 Yuta Ike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RefrigeratorRecipeRxTests/RefrigeratorRecipeRxTests.swift
@@ -0,0 +1,34 @@
+//
+//  RefrigeratorRecipeRxTests.swift
+//  RefrigeratorRecipeRxTests
+//
+//  Created by 池友太 on 2019/04/23.
+//  Copyright © 2019 Yuta Ike. All rights reserved.
+//
+
+import XCTest
+@testable import RefrigeratorRecipeRx
+
+class RefrigeratorRecipeRxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
