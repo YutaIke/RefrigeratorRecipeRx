40ec50228365ad99332f62fce6fcd14b524915c4
YutaIke <kei.yt22@gmail.com>
2019-04-25 00:09:33 +0900

    初期画面変更

---
+++ RefrigeratorRecipeRx/Presentation/UI/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  RefrigeratorRecipeRx
+//
+//  Created by 池友太 on 2019/04/24.
+//  Copyright © 2019 Yuta Ike. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
