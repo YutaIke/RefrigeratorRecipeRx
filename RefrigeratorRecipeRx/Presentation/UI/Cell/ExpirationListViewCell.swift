011e0cf170c8b036f41869d604a6b1f3094875dc
YutaIke <kei.yt22@gmail.com>
2019-04-24 23:42:56 +0900

    ExpirationListViewCell作成

---
+++ RefrigeratorRecipeRx/Presentation/UI/Cell/ExpirationListViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ExpirationListViewCell.swift
+//  RefrigeratorRecipeRx
+//
+//  Created by 池友太 on 2019/04/24.
+//  Copyright © 2019 Yuta Ike. All rights reserved.
+//
+
+import UIKit
+
+class ExpirationListViewCell: UITableViewCell {
+    
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var expirationDate: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
