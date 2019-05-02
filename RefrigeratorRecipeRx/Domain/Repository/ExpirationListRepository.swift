a107c0cf8c2a058ea7b38223e1f487bbc70ae289
YutaIke <kei.yt22@gmail.com>
2019-05-02 13:39:58 +0900

    Repositoryのsave処理に新しいIDを採番する処理を追加

--- RefrigeratorRecipeRx/Domain/Repository/ExpirationListRepository.swift
+++ RefrigeratorRecipeRx/Domain/Repository/ExpirationListRepository.swift
@@ -8,9 +8,12 @@
 
 import Foundation
 import RxSwift
+import RealmSwift
 
 protocol ExpirationListRepositoryProtocol {
+    func save(_ expirationItem: ExpirationItemEntity)
     func getAllExpirationItemList() -> Observable<[ExpirationItemEntity]>
+    func createNewId() -> Int
 }
 
 struct ExpirationListRepository: ExpirationListRepositoryProtocol {
@@ -20,7 +23,18 @@ struct ExpirationListRepository: ExpirationListRepositoryProtocol {
         self.dataStore = dataStore
     }
     
+    func save(_ expirationItem: ExpirationItemEntity) {
+        expirationItem.id = createNewId()
+        dataStore.save(expirationItem)
+    }
+    
     func getAllExpirationItemList() -> Observable<[ExpirationItemEntity]> {
         return dataStore.getAllExpirationItems()
+    }
+    
+    // 新しいIDを採番する。
+    internal func createNewId() -> Int {
+        let realm = try! Realm()
+        return (realm.objects(ExpirationItemEntity.self).sorted(byKeyPath: "id", ascending: false).first?.id ?? 0) + 1
     }
 }
